cd324b8839b4b187b52bb55fbe39c41abba70f2b
MC Hobbit <i.am.mchobbit@gmail.com>
2025-02-04 00:18:02 +0100

    added decaying cache Class

--- src/my/classes/__init__.py
+++ src/my/classes/__init__.py
@@ -1,2 +1,24 @@
 '''
 '''
+
+import time
+
+
+class MyTTLCache:
+
+    def __init__(self, ttl):
+        self.ttl = ttl
+        self.cache = {}
+
+    def set(self, key, value):
+        self.cache[key] = {'value': value, 'time': time.time()}
+
+    def get(self, key):
+        if key in self.cache:
+            entry = self.cache[key]
+            if time.time() - entry['time'] < self.ttl:
+                return entry['value']
+            else:
+                del self.cache[key]
+        return None
+
