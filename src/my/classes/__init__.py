0e0cc13f7dd0313ba4ffe8a6c630f9e4c21b1f4b
MC Hobbit <i.am.mchobbit@gmail.com>
2025-02-04 15:16:36 +0100

    Updated docs

--- src/my/classes/__init__.py
+++ src/my/classes/__init__.py
@@ -1,19 +1,88 @@
-'''
-'''
+# -*- coding: utf-8 -*-
+"""Misc classes that Prate uses.
+
+Created on Feb 4, 2025
+
+@author: mchobbit
+
+This module contains miscellaneous classes that Prate uses. For example, MyTTLCache
+offers a cache whose data expires (ceases to be cached) after N seconds. This offers
+advantages over traditional caching, especially... Anyway, see below.
+
+Example:
+    Here is how to use one of these classes, for example::
+
+    $ python3.12
+    >>> from time import sleep
+    >>> from my.clasess import MyTTLCache
+    >>> cache = MyTTLCache(10)
+    >>> cache.set('foo', 69)
+    >>> cache.get('foo')
+    69
+    >>> sleep(3)
+    >>> cache.get('foo')
+    69
+    >>> sleep(7)
+    >>> cache.get('foo')
+    >>>
+
+    You get the idea.
+
+Now, here is a section break. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    None.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+.. _Napoleon Style Guide:
+   https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+
+"""
 
 import time
 
 
 class MyTTLCache:
+    """Cache with self-expiring data after N seconds.
+
+    This class offers a cache whose data self-expires after N seconds. That
+    way, stale data is automatically dropped from the cache.
+
+    This offers advantages when one is coding for pings, whois calls, and
+    other network-related things.
+
+    Attributes:
+        attr1 (str): Description of `attr1`.
+        attr2 (:obj:`int`, optional): Description of `attr2`.
+
+    """
 
     def __init__(self, ttl):
         self.ttl = ttl
         self.cache = {}
 
     def set(self, key, value):
+        """Set cache value."""
         self.cache[key] = {'value': value, 'time': time.time()}
 
     def get(self, key):
+        """Get cache value.
+
+        Example:
+            >>> cache = MyTTLCache(10)
+            >>> cache.set('foo', 69)
+            >>> cache.get('foo')
+            69
+            >>> sleep(10); cache.get('foo')
+            >>>
+        """
         if key in self.cache:
             entry = self.cache[key]
             if time.time() - entry['time'] < self.ttl:
