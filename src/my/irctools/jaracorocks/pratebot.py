9493345ff9731be84a7980caeed8e70e3adc7f3a
MC Hobbit <i.am.mchobbit@gmail.com>
2025-02-09 00:30:06 +0100

    complete rewrite

--- src/my/irctools/jaracorocks/pratebot.py
+++ src/my/irctools/jaracorocks/pratebot.py
@@ -26,185 +26,51 @@ Todo:
 Example:
 
 from Crypto.PublicKey import RSA
-from pratebot16 import *
-import queue
-desired_nickname = 'mac1'
+from my.irctools.jaracorocks.vanilla import BotForDualQueuedSingleServerIRCBotWithWhoisSupport
+desired_nickname = 'mac2'
 my_irc_server = 'cinqcent.local'
 my_channel = '#prate'
-rx_q = queue.LifoQueue()
-tx_q = queue.LifoQueue()
-my_rsa_key = RSA.generate(2048)
-bot = PrateBot(channel=my_channel, nickname=desired_nickname,
-                rsa_key=my_rsa_key,
-                irc_server=my_irc_server,
-                port=6667,
-                startup_timeout=30)
+my_port = 6667
+bot = BotForDualQueuedSingleServerIRCBotWithWhoisSupport(my_channel, desired_nickname, my_irc_server, my_port)
+while not bot.ready:
+    sleep(1)
+
+bot.put("mac1", "WORD")
+bot.get()
 
 """
 
 import sys
-from time import sleep
 from threading import Thread
 
-from my.classes.readwritelock import ReadWriteLock
-from _queue import Empty
-from random import randint, choice, shuffle
+# from my.classes.readwritelock import ReadWriteLock
+from random import randint, shuffle
 from Crypto.PublicKey import RSA
-from my.irctools.jaracorocks import PersistentCryptoOrientedSingleServerIRCBotWithWhoisSupport
-from queue import LifoQueue
 from my.stringtools import generate_irc_handle
 from my.classes.exceptions import MyIrcFingerprintMismatchCausedByServer, MyIrcInitialConnectionTimeoutError
-from my.globals import JOINING_IRC_SERVER_TIMEOUT
+from my.irctools.jaracorocks.vanilla import BotForDualQueuedSingleServerIRCBotWithWhoisSupport
+from time import sleep
 # from my.globals import JOINING_IRC_SERVER_TIMEOUT, PARAGRAPH_OF_ALL_IRC_NETWORK_NAMES
 
 
-class PrateBot:
+class PrateBot(BotForDualQueuedSingleServerIRCBotWithWhoisSupport):
 
-    def __init__(self, channel, nickname, rsa_key, irc_server, port, startup_timeout,
-                 maximum_reconnections=None):
-        self.__startup_timeout = startup_timeout
-        self.__crypto_rx_queue = LifoQueue()
-        self.__crypto_tx_queue = LifoQueue()
-        self.__should_we_quit = False
-        self.__should_we_quit_lock = ReadWriteLock()
-        self.channel = channel
-        self.nickname = nickname
+    def __init__(self, channel, nickname, irc_server, port, rsa_key,
+                 startup_timeout=40, maximum_reconnections=None):
+        super().__init__(channel, nickname, irc_server, port, startup_timeout, maximum_reconnections)
         self.rsa_key = rsa_key
-        self.irc_server = irc_server
-        self.port = port
-        self.svr = None  # Set by self.maintain_server_connection()
-        self.__maximum_reconnections = maximum_reconnections
-        self.__should_we_quit = False
-        self.__autoreconnect = True  # Set to False to suspend autoreconnection. Set to True to resume autoreconnecting.
-        self.__autoreconnect_lock = ReadWriteLock()
-        self.__noof_reconnections = 0
-        self.__noof_reconnections_lock = ReadWriteLock()
-        self.__my_main_thread = Thread(target=self._start, daemon=True)
+        assert(not hasattr(self, '_bot_start'))
+        assert(not hasattr(self, '__my_main_thread'))
+        self.__my_main_thread = Thread(target=self._bot_loop, daemon=True)
         self.__my_main_thread.start()
 
-    @property
-    def noof_reconnections(self):
-        self.__noof_reconnections_lock.acquire_read()
-        try:
-            retval = self.__noof_reconnections
-            return retval
-        finally:
-            self.__noof_reconnections_lock.release_read()
-
-    @noof_reconnections.setter
-    def noof_reconnections(self, value):
-        self.__noof_reconnections_lock.acquire_write()
-        try:
-            self.__noof_reconnections = value
-        finally:
-            self.__noof_reconnections_lock.release_write()
-
-    @property
-    def startup_timeout(self):
-        return self.__startup_timeout
-
-    @property
-    def crypto_rx_queue(self):
-        return self.__crypto_rx_queue
-
-    @property
-    def crypto_tx_queue(self):
-        return self.__crypto_tx_queue
-
-    def _start(self):
-        while not self.should_we_quit and (self.maximum_reconnections is None or self.noof_reconnections < self.maximum_reconnections):
-            sleep(1)
-            if self.svr is None and self.autoreconnect:
-                self.reconnect_server_connection()  # If its fingerprint is wonky, quit&reconnect.
-#           self.process_data_input_and_output()  # if self.svr and self.svr.ready [not written yet] :)
-        if self.maximum_reconnections is not None and self.noof_reconnections >= self.maximum_reconnections:
-            print("We've reconnected %d times. That's enough. It's over. This connection has died and I'll not resurrect it. Instead, I'll wait until this bot is told to quit; then, I'll exit/join/whatever.")
+    def _bot_loop(self):
+        print("_start() --- started")
         while not self.should_we_quit:
-            sleep(1)
+            sleep(.1)
+            # Process incoming/outgoing messages, for interfacing with our homies
         print("Quitting. Huzzah.")
-
-    @property
-    def autoreconnect(self):
-        self.__autoreconnect_lock.acquire_read()
-        try:
-            retval = self.__autoreconnect
-            return retval
-        finally:
-            self.__autoreconnect_lock.release_read()
-
-    @autoreconnect.setter
-    def autoreconnect(self, value):
-        self.__autoreconnect_lock.acquire_write()
-        try:
-            self.__autoreconnect = value
-        finally:
-            self.__autoreconnect_lock.release_write()
-
-    @property
-    def should_we_quit(self):
-        self.__should_we_quit_lock.acquire_read()
-        try:
-            retval = self.__should_we_quit
-            return retval
-        finally:
-            self.__should_we_quit_lock.release_read()
-
-    @should_we_quit.setter
-    def should_we_quit(self, value):
-        self.__should_we_quit_lock.acquire_write()
-        try:
-            self.__should_we_quit = value
-        finally:
-            self.__should_we_quit_lock.release_write()
-
-    @property
-    def maximum_reconnections(self):
-        return self.__maximum_reconnections
-
-    def reconnect_server_connection(self):
-        while self.svr is None and self.autoreconnect:
-            try:
-                print("*** CONNECTING TO %s AS %s ***" % (self.irc_server, self.nickname))
-                self.generate_new_svr()
-            except MyIrcFingerprintMismatchCausedByServer:
-                print("%s changed my nickname and didn't tell me. I must disconnect and reconnect, so as to refresh my fingerprint." % self.irc_server)
-                try:
-                    self.svr.disconnect("Bye")
-                except Exception as e:
-                    print("Exception occurred (which we shall ignore):", e)
-                if self.svr:
-                    self.svr.shut_down_threads()
-                self.svr = None
-                self.nickname = "%s%d" % (generate_irc_handle(), randint(1000, 9999))
-            except MyIrcInitialConnectionTimeoutError:
-                print("Timed out while trying to connect to server.")
-            else:
-                if self.channel not in self.svr.channels:
-                    print("WARNING -- we dropped out of %s" % self.channel)
-                    self.svr.connection.join(self.channel)
-                else:
-                    print("YAY")
-#                    pass
-
-    def quit(self):  # Do we need this?
-        """Quit this bot."""
-        self.autoreconnect = False
-        self.should_we_quit = True
-        if self.svr:
-            self.svr.shut_down_threads()
-        self.__my_main_thread.join()  # print("Joining server thread")
-
-    def generate_new_svr(self):
-        self.noof_reconnections += 1
-        self.svr = PersistentCryptoOrientedSingleServerIRCBotWithWhoisSupport(
-            channel=self.channel,
-            nickname=self.nickname,
-            rsa_key=self.rsa_key,
-            irc_server=self.irc_server,
-            port=self.port,
-            crypto_rx_queue=self.crypto_rx_queue,
-            crypto_tx_queue=self.crypto_tx_queue,
-            startup_timeout=self.startup_timeout)
+        # self.join() # TODO: Do I need this?
 
 
 class HaremOfBots:
@@ -251,8 +117,8 @@ class HaremOfBots:
         for k in self.list_of_all_irc_servers:
             print("Trying", k)
             self.try_to_log_into_this_IRC_server(k)
-        failures = lambda: [k for k in self.bots if self.bots[k].noof_reconnections >= 3 and not self.bots[k].svr]
-        successes = lambda: [k for k in self.bots if self.bots[k].svr and self.bots[k].svr.joined]
+        failures = lambda: [k for k in self.bots if self.bots[k].noof_reconnections >= 3 and not self.bots[k].client]
+        successes = lambda: [k for k in self.bots if self.bots[k].client and self.bots[k].client.joined]
         while len(failures()) + len(successes()) < len(self.bots):
             sleep(1)
         for k in list(failures()):
@@ -266,10 +132,9 @@ class HaremOfBots:
         try:
             self.bots[k] = PrateBot(channel=self.channel,
                                    nickname=self.desired_nickname,
-                                   rsa_key=self.rsa_key,
                                    irc_server=k,
                                    port=self.port,
-                                   startup_timeout=JOINING_IRC_SERVER_TIMEOUT)
+                                   rsa_key=self.rsa_key)
         except (MyIrcInitialConnectionTimeoutError, MyIrcFingerprintMismatchCausedByServer):
             self.bots[k] = None
 
@@ -288,29 +153,8 @@ if __name__ == "__main__":
         my_channel = sys.argv[3]
         desired_nickname = sys.argv[4]
 
-    my_crypto_rx_queue = LifoQueue()
-    my_crypto_tx_queue = LifoQueue()
     my_rsa_key = RSA.generate(2048)
-    bot = PrateBot(my_channel, desired_nickname, my_rsa_key,
-                   my_irc_server, my_port,
-                   startup_timeout=JOINING_IRC_SERVER_TIMEOUT)
-    while True:
+    bot = PrateBot(my_channel, desired_nickname, my_irc_server, my_port, my_rsa_key)
+    while not bot.ready:
         sleep(1)
-        if bot.svr is None:
-            continue
-        bot.svr.show_users_dct_info(True if randint(0, 10) == 0 else False)
-        try:
-            u = choice(list(bot.svr.homies.keys()))
-        except IndexError:
-            pass
-        else:
-            if bot.svr.homies[u].ipaddr is not None and randint(0, 10) == 0:
-                bot.crypto_tx_queue.put((u, ('HELLO from %s to %s' % (bot.svr.nickname, u)).encode()))
-            try:
-                while True:
-                    the_user, the_blk = bot.crypto_rx_queue.get_nowait()
-                    assert("HELLO from" in the_blk.decode())
-                    print(the_user, "==>", the_blk)
-            except Empty:
-                pass
 
