78d1953c0ff738f3eddafa3ff3a5cf62438503d8
MC Hobbit <i.am.mchobbit@gmail.com>
2025-02-15 23:11:35 +0100

    Disabled some junk

--- src/sheepdip.py
+++ src/sheepdip.py
@@ -15,82 +15,35 @@ from my.globals import PARAGRAPH_OF_ALL_IRC_NETWORK_NAMES, JOINING_IRC_SERVER_TI
 from my.classes.exceptions import MyIrcInitialConnectionTimeoutError, MyIrcFingerprintMismatchCausedByServer
 from time import sleep
 from queue import Queue
-'''
 
+
+import os
+import socket
+from my.irctools.jaracorocks.harem import HaremOfPrateBots
 from Crypto.PublicKey import RSA
 from time import sleep
-from my.globals import ALL_IRC_NETWORK_NAMES
-from my.stringtools import generate_random_alphanumeric_string
-from my.irctools.jaracorocks.vanilla import BotForDualQueuedSingleServerIRCBotWithWhoisSupport
-from queue import Empty
-from random import randint
-from IPython.testing.plugin.dtexample import random_all
+from my.stringtools import *
+from my.globals import *
+import datetime
+my_rsa_key = RSA.generate(2048)
+nickname = socket.gethostname()
+the_room = "#prate%d" % datetime.datetime.now().day
 
+harem = HaremOfPrateBots(['#prate'], nickname, ALL_SANDBOX_IRC_NETWORK_NAMES, my_rsa_key)
+buddies = harem.users
 
-def join_as_many_irc_servers_as_possible(channel, desired_nickname, my_port=6667):
-    bots = {}
-    for my_irc_server in ALL_IRC_NETWORK_NAMES:
-        bots[my_irc_server] = BotForDualQueuedSingleServerIRCBotWithWhoisSupport([channel], desired_nickname, my_irc_server, my_port)
-    return bots
+h2 = HaremOfPrateBots(['#prate'], 'mac2222', ALL_IRC_NETWORK_NAMES, my_rsa_key2)
+while len(h1.ready_bots(my_rsa_key2.public_key())) < 3 and len(h2.ready_bots(my_rsa_key2.public_key())) < 3:
+    sleep(A_TICK)
 
+h1.ready_bots(my_rsa_key2.public_key())
+for _ in range(0, 10):
+    h1.put(my_rsa_key2.public_key(), b"HELLO WORLD")
+    h2.get() == (my_rsa_key1.public_key(), b"HELLO WORLD")
 
-Xbots = join_as_many_irc_servers_as_possible('#prate', 'xmchob%d' % randint(100, 999))
-Ybots = join_as_many_irc_servers_as_possible('#prate', 'ymchob%d' % randint(100, 999))
-successes_thus_far = -1
-while successes_thus_far < len([k for k in ALL_IRC_NETWORK_NAMES if Xbots[k].ready and Ybots[k].ready]):
-    successes_thus_far = len([k for k in ALL_IRC_NETWORK_NAMES if Xbots[k].ready and Ybots[k].ready])
-    sleep(10)
+with open("/Users/mchobbit/Downloads/pi_holder.stl", "rb") as f:
+    h1.put(my_rsa_key2.public_key(), f.read())
 
-goodKs = [k for k in ALL_IRC_NETWORK_NAMES if Xbots[k].ready and Ybots[k].ready]
+pk, dat = h2.get()
 
-for k in goodKs:
-    for xy in (Xbots, Ybots):
-        try:
-            while True:
-                _ = xy[k].get_nowait()
-        except Empty:
-            break
-
-stem = generate_random_alphanumeric_string(30)
-for k in goodKs:
-    if Xbots[k].ready and Ybots[k].ready:
-        p = stem
-        Xbots[k].put(Ybots[k].nickname, p)
-
-defective_items = []
-for k in goodKs:
-    if Xbots[k].ready and Ybots[k].ready:
-        p = stem
-        try:
-            (src, msg) = Ybots[k].get(timeout=2)
-            if p == msg:
-                print("%s is gooood" % k)
-            else:
-                print("%s is defective" % k)
-                defective_items += [k]
-        except:
-            print("No response from %s" % k)
-
-stem = generate_random_alphanumeric_string(30)
-for k in goodKs:
-    if Ybots[k].ready and Ybots[k].ready:
-        p = stem
-        Ybots[k].put(Xbots[k].nickname, p)
-
-defective_items = []
-for k in goodKs:
-    if Ybots[k].ready and Xbots[k].ready:
-        p = stem
-        try:
-            (src, msg) = Xbots[k].get(timeout=2)
-            if p == msg:
-                print("%s is gooood" % k)
-            else:
-                print("%s is defective" % k)
-                defective_items += [k]
-        except:
-            print("No response from %s" % k)
-
-for k in defective_items:
-    goodKs.remove(k)
-
+'''
