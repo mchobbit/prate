fcd77964252b6417332913fb41a4fff00f27ee8b
MC Hobbit <i.am.mchobbit@gmail.com>
2025-02-18 22:29:28 +0100

    sheepdip

--- src/sheepdip.py
+++ src/sheepdip.py
@@ -6,44 +6,48 @@ Created on Jan 21, 2025
 
 # print(key.get_base64())  # print public key
 # key.write_private_key(sys.stdout)
-
+'''
 
 from random import shuffle, randint
 from my.stringtools import generate_irc_handle
 from Crypto.PublicKey import RSA
-from my.globals import PARAGRAPH_OF_ALL_IRC_NETWORK_NAMES, JOINING_IRC_SERVER_TIMEOUT
-from my.classes.exceptions import MyIrcInitialConnectionTimeoutError, MyIrcFingerprintMismatchCausedByServer
+from my.globals import PARAGRAPH_OF_ALL_IRC_NETWORK_NAMES
 from time import sleep
 from queue import Queue
 
-
-import os
-import socket
-from my.irctools.jaracorocks.harem import HaremOfPrateBots
 from Crypto.PublicKey import RSA
 from time import sleep
 from my.stringtools import *
 from my.globals import *
+import os
+import socket
 import datetime
-my_rsa_key = RSA.generate(2048)
-nickname = socket.gethostname()
-the_room = "#prate%d" % datetime.datetime.now().day
+from my.irctools.cryptoish import squeeze_da_keez
+from my.irctools.jaracorocks.harem import HaremOfPrateBots
+my_nickname = socket.gethostname().replace('.', '_')[:MAX_NICKNAME_LENGTH]
+the_room = "#prattling"
+alice_rsa_key = RSA.generate(2048)
+bob_rsa_key = RSA.generate(2048)
+the_irc_server_URLs = ALL_SANDBOX_IRC_NETWORK_NAMES
+alice_harem = HaremOfPrateBots([the_room], my_nickname, the_irc_server_URLs, alice_rsa_key)
+bob_harem = HaremOfPrateBots([the_room], my_nickname, the_irc_server_URLs, bob_rsa_key)
+print("Opening harems")
+while len(alice_harem.users) < 2 and len(bob_harem.users) < 2:
+    sleep(1)
 
-harem = HaremOfPrateBots(['#prate'], nickname, ALL_SANDBOX_IRC_NETWORK_NAMES, my_rsa_key)
-buddies = harem.users
+while len(alice_harem.ipaddrs) < 1 and len(bob_harem.ipaddrs) < 1:
+    sleep(1)
 
-h2 = HaremOfPrateBots(['#prate'], 'mac2222', ALL_IRC_NETWORK_NAMES, my_rsa_key2)
-while len(h1.ready_bots(my_rsa_key2.public_key())) < 3 and len(h2.ready_bots(my_rsa_key2.public_key())) < 3:
-    sleep(A_TICK)
+sleep(5)
+print("Sending a file from %s to %s" % (alice_harem.desired_nickname, bob_harem.desired_nickname))
+alice_harem.put(bob_rsa_key.public_key(), b"HELLO WELLO")
+src, msg = bob_harem.get()
 
-h1.ready_bots(my_rsa_key2.public_key())
-for _ in range(0, 10):
-    h1.put(my_rsa_key2.public_key(), b"HELLO WORLD")
-    h2.get() == (my_rsa_key1.public_key(), b"HELLO WORLD")
-
+t = datetime.datetime.now()
 with open("/Users/mchobbit/Downloads/pi_holder.stl", "rb") as f:
-    h1.put(my_rsa_key2.public_key(), f.read())
+    alice_harem.put(bob_rsa_key.public_key(), f.read())
 
-pk, dat = h2.get()
+u = datetime.datetime.now()
 
-'''
+pk, dat = bob_harem.get()
+pass
